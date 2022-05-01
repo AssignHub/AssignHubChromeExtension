7ad69ff6fb256374838af90deaca109bf8d20d40
Jonathan Liu <liu.z.jonathan@gmail.com>
2022-05-01 11:40:01 -0700

    parse all classes

--- js/parse-classes.js
+++ js/parse-classes.js
@@ -4,42 +4,52 @@ const parsedClasses = document.getElementById('parsed-classes')
 
 /* Function definitions */
 function parseClasses() {
-  // TODO: get the term from the user's currently selected term (maybe a dropdown in popup.html?)
-  const term = '20221'
+  /* Parses all classes from all the terms available on my.usc.edu */
+  
+  // Get all the terms by looking at the href of the tab elements
+  const terms = [...document.querySelector('.courses-data > .tabs').children].map(tab => {
+    const href = tab.children[0].href
+    return href.substring(href.lastIndexOf('#')+1)
+  })
 
-  const classes = []
-  const classesHTML = document.getElementById(`term-${term}`).querySelector('.list').children
+  const classes = {}
+  for (const termId of terms) {
+    // Check if term exists in html
+    const termHTML = document.getElementById(termId)
+    if (!termHTML) continue
 
-  // Iterate through the user's classes
-  for (const c of classesHTML) {
-    let course, sections
+    classes[termId] = []
+    const classesHTML = termHTML.querySelector('.list').children
 
-    // Find the child that contains data about the course
-    for (const child of c.children) {
-      if (child.dataset.hasOwnProperty('course') && child.dataset.hasOwnProperty('sections')) {
-        course = child.dataset.course
-        sections = child.dataset.sections
-        break
+    // Iterate through the user's classes
+    for (const c of classesHTML) {
+      let course, sections
+
+      // Find the child that contains data about the course
+      for (const child of c.children) {
+        if (child.dataset.hasOwnProperty('course') && child.dataset.hasOwnProperty('sections')) {
+          course = child.dataset.course
+          sections = child.dataset.sections
+          break
+        }
       }
-    }
 
-    // If we couldn't find course or sections
-    if (!course || !sections) continue
-    
-    // Format course id and sections
-    const courseSplit = course.split(' ')
-    const courseId = `${courseSplit[0]}-${courseSplit[1]}`
-    const sectionsSplit = sections.split(',')
-    const _class = {
-      courseId,
-      mainSection: sectionsSplit[0],
-      otherSections: sectionsSplit.slice(1, sectionsSplit.length),
-    }
+      // If we couldn't find course or sections
+      if (!course || !sections) continue
+      
+      // Format course id and sections
+      const courseSplit = course.split(' ')
+      const courseId = `${courseSplit[0]}-${courseSplit[1]}`
+      const sectionsSplit = sections.split(',')
+      const _class = {
+        courseId,
+        mainSection: sectionsSplit[0],
+        otherSections: sectionsSplit.slice(1, sectionsSplit.length),
+      }
 
-    classes.push(_class)
+      classes[termId].push(_class)
+    }
   }
-
-  //console.log(classes)
 
   chrome.runtime.sendMessage({ type: 'parse-classes', classes })
 }
@@ -54,14 +64,48 @@ parseBtn.addEventListener('click', async () => {
   })
 })
 
-chrome.runtime.onMessage.addListener((message) => {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log('MESSAGE: ', message)
   switch (message.type) {
     case 'parse-classes':
-      for (const c of message.classes) {
-        parsedClasses.innerHTML += `<li>Course ID: ${c.courseId}, Section ${c.mainSection}</li>`
+      for (const term of Object.keys(message.classes)) {
+        parsedClasses.innerHTML += `<h3>${term}</h3>`
+        for (const c of message.classes[term]) {
+          parsedClasses.innerHTML += `<li>Course ID: ${c.courseId}, Section ${c.mainSection}, Other sections: ${c.otherSections}</li>`
+        }
       }
       break
   }
-
+  sendResponse(true)
 })
+
+/*
+
+chrome.tabs.onActivated.addListener(({ tabId }) => {
+  window.activeTabId = tabId
+})
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (
+    tabId === window.activeTabId &&
+    changeInfo.url &&
+    changeInfo.url === step1Data[window.userData.school].url
+  ) {
+    console.log('YES')
+    step1.innerHTML = 'DONE'
+  }
+})
+*/
+
+/* Onload */
+/*
+chrome.tabs.query({ active: true, currentWindow: true }).then(({ id, url }) => {
+  window.activeTabId = id
+
+  if (url === step1Data['usc'].url) {
+    console.log('YES')
+    // This doesn't work because it's being overridden by the stuff in auth.js once user data is updated 
+    document.getElementById('step-1').innerHTML = 'DONE'
+  }
+})
+*/
